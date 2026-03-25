d168c0488b4b4bdceb07b2d5e5c7d9f889bd0658
Hugo <hugo.garcia.sanchez@estudiantat.upc.edu>
2026-03-25 18:11:09 +0100

    data items of cat021 added

---
+++ asterix_decoder/data_items/CAT021/item_021_131.py
@@ -0,0 +1,29 @@
+﻿from asterix_decoder.data_items.length_type import LengthType, extract_octets
+from asterix_decoder.data_items.data_item import DataItem
+
+
+class Item131(DataItem):
+
+    @staticmethod
+    def get_item_id() -> str:
+        return "I021/131"
+
+    '''
+        Name:       Position in WGS-84 co-ordinates, high res.
+        Definition: Pending implementation according to CAT021 specification.
+        Format:     8 octets.
+    '''
+
+    def __init__(self, item_name: str, length_str: str):
+        super().__init__(item_name, length_str)
+        self.data = {
+            "RAW_HEX": None,
+        }
+
+    @extract_octets
+    def decode(self, octets: bytes) -> dict[str, any]:
+        return self._bits_to_data(self.data.copy(), octets)
+
+    def _bits_to_data(self, data, octets: bytes) -> dict[str, any]:
+        data["RAW_HEX"] = octets.hex().upper()
+        return data
---
+++ asterix_decoder/data_items/CAT021/item_021_145.py
@@ -0,0 +1,29 @@
+﻿from asterix_decoder.data_items.length_type import LengthType, extract_octets
+from asterix_decoder.data_items.data_item import DataItem
+
+
+class Item145(DataItem):
+
+    @staticmethod
+    def get_item_id() -> str:
+        return "I021/145"
+
+    '''
+        Name:       Flight Level
+        Definition: Pending implementation according to CAT021 specification.
+        Format:     2 octets.
+    '''
+
+    def __init__(self, item_name: str, length_str: str):
+        super().__init__(item_name, length_str)
+        self.data = {
+            "RAW_HEX": None,
+        }
+
+    @extract_octets
+    def decode(self, octets: bytes) -> dict[str, any]:
+        return self._bits_to_data(self.data.copy(), octets)
+
+    def _bits_to_data(self, data, octets: bytes) -> dict[str, any]:
+        data["RAW_HEX"] = octets.hex().upper()
+        return data
